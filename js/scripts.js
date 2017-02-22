9ff0ae388489a8e603e167d60040243b76d00602
mickijames <mwilliams27@vcd-ch204-14.eastern.ewu.edu>
2017-02-22 13:44:59 -0800

    Randomized Image
    * Random homepage images
    * JavaScript to randomize, not CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-// YOUR SCRIPTS GO HERE
+// Store 3 images
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get random number
+var max = 3;
+var randNum = Math.floor(Math.random() * max);
+
+// Display randomized images
+$('img').attr('src', eval('img' + randNum));
