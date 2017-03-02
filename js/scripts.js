87bc0593ded2bc814aa8c2d2231e3be353f0abd6
mickijames <micalynjeanette@eagles.ewu.edu>
2017-03-02 11:11:53 -0800

    Removed pictures
    * Made less images to randomize

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,13 @@ var img0 = '../img/castle-w-fireworks.jpg';
 var img1 = '../img/classic-castle.jpg';
 var img2 = '../img/lilo-&-stitch.jpg';
 var img3 = '../img/lion-king.jpg';
-var img4 = '../img/mickey-gang.jpg';
-var img5 = '../img/monsters-inc.jpg';
-var img6 = '../img/mulan.png';
-var img7 = '../img/pirates.jpg';
-var img8 = '../img/starwars.jpg';
-var img9 = '../img/statue-of-walt.jpg';
+var img4 = '../img/mulan.png';
+var img5 = '../img/pirates.jpg';
+var img6 = '../img/starwars.jpg';
+var img7 = '../img/statue-of-walt.jpg';
 
 // Get random number
-var max = 10;
+var max = 8;
 var randNum = Math.floor(Math.random() * max);
 
 // Display randomized images
